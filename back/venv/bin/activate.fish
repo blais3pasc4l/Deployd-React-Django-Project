a7432fc5de1cb2751ab732f78942e7f91d85ae4e
Juan David Calderon <juandavidcalderonpena@gmail.com>
2022-03-01 18:09:04 -0500

    update

--- back/venv/bin/activate.fish
+++ back/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/pascal/workspace/pruebaTecnica/backend/venv"
+set -gx VIRTUAL_ENV "/home/pascal/workspace/pruebaTecnica/back/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
